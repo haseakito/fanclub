1658c2cec03286560c71340d342c03e94b67eaeb
haseakito <haseakito@gmail.com>
2024-02-29 09:29:22 +1100

    :sparkles: Add `alert-modal` for confirmation

---
+++ components/modals/alert-modal.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { Modal } from "@/components/ui/modal";
+import { Button } from "@/components/ui/button";
+
+interface AlertModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  loading: boolean;
+}
+
+export const AlertModal: React.FC<AlertModalProps> = ({
+  isOpen,
+  onClose,
+  onConfirm,
+  loading,
+}) => {
+  // Boolean state handling component mount state
+  const [isMounted, setIsMounted] = useState(false);
+
+  // Lifecycle handling setting component mount state
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Modal
+      title="Are you sure?"
+      description="This action cannot be undone. Please make sure before you proceed."
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="pt-3 space-x-2 flex items-center justify-end w-full">
+        <Button disabled={loading} variant="outline" onClick={onClose}>
+          Cancel
+        </Button>
+        <Button disabled={loading} variant="destructive" onClick={onConfirm}>
+          Continue
+        </Button>
+      </div>
+    </Modal>
+  );
+};
