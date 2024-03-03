afc992dde65e9e8f59311b2d73d9eac709123b7c
haseakito <haseakito@gmail.com>
2024-03-04 10:04:41 +1100

    :art: Modified linting and spacing

--- hooks/use-post-modal.tsx
+++ hooks/use-post-modal.tsx
@@ -1,13 +1,13 @@
-import { create } from "zustand"
+import { create } from "zustand";
 
 interface usePostModalStore {
-    isOpen: boolean,
-    onOpen: () => void,
-    onClose: () => void
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
 }
 
 export const usePostModal = create<usePostModalStore>((set) => ({
-    isOpen: false,
-    onOpen: () => set({ isOpen: true }),
-    onClose: () => set({ isOpen: false })
-}))
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
