8563f41564c2f0ec9796754a3f0256c07a1ea366
haseakito <haseakito@gmail.com>
2024-03-07 11:47:45 +1100

    :wrench: Add image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com", "images.clerk.dev"],
+  },
+};
 
 export default nextConfig;
