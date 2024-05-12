5796e2d262be3bc0341c2bf5b6247cc8c394561f
haseakito <haseakito@gmail.com>
2024-05-12 16:45:26 +1000

    :sparkles: Remove `@clerk/nextjs` and add session manager instead

--- app/(home)/components/nav-items.tsx
+++ app/(home)/components/nav-items.tsx
@@ -1,27 +1,32 @@
 "use client";
 
-import Link from "next/link";
+import axios from "axios";
 import { cn } from "@/lib/utils";
-import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import toast from "react-hot-toast";
+
+import { Button } from "@/components/ui/button";
 import {
-  MessageCircleMore,
-  Settings,
-  LogOut,
   User,
   Mail,
+  Heart,
+  Settings,
   Banana,
-  LayoutDashboard,
+  LogOut,
+  LogIn,
 } from "lucide-react";
-import { SignOutButton } from "@clerk/nextjs";
-import { SignedIn } from "@clerk/clerk-react";
-import { Button } from "@/components/ui/button";
 
-export const HomeNavigationMenu = () => {
-  // Hooks handling current URL's pathname
-  const pathname = usePathname();
+export const HomeNavigationMenu = ({ signedIn }: { signedIn: boolean }) => {
+  // Boolean state handling loading during API request
+  const [loading, setLoading] = useState(false);
 
   // Hooks handling router
-  const router = useRouter()
+  const router = useRouter();
+
+  // Hooks handling current URL's pathname
+  const pathname = usePathname();
 
   // Navigation item, its location, and its current status
   const navItems: {
@@ -43,18 +48,38 @@ export const HomeNavigationMenu = () => {
       active: pathname === `/messages`,
     },
     {
-      href: `/comments`,
-      label: "Comments",
-      icon: <MessageCircleMore className="h-5 w-5" />,
+      href: `/likes`,
+      label: "Likes",
+      icon: <Heart className="h-5 w-5" />,
       active: pathname === `/comments`,
     },
     {
       href: `/settings`,
       label: "Settings",
       icon: <Settings className="h-5 w-5" />,
-      active: pathname === `/settings`,
+      active: pathname.startsWith("/settings"),
     },
   ];
+
+  // Function handling deleting the cookie session
+  const onLogout = async () => {
+    try {
+      setLoading(true);
+
+      // DELETE request to proxy server
+      await axios.delete("/api/auth/sign-out");
+
+      // Show a success toast
+      toast.success("Successfully logged out!");
+
+      // Redirect the user to login page
+      router.push("/auth/sign-in");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <nav className="fixed top-0 left-0 z-40 w-16 md:w-52 h-full transition-transform -translate-x-full sm:translate-x-0">
@@ -86,23 +111,25 @@ export const HomeNavigationMenu = () => {
           ))}
         </div>
         <div className="mt-60">
-          <SignedIn>
-            <SignOutButton>
-              <Button variant="destructive" className="md:w-full">
-                <p className="hidden md:block">Sign out</p>
-                <LogOut className="h-5 w-5 md:hidden" />
-              </Button>
-            </SignOutButton>
+          {signedIn ? (
             <Button
-              variant="link"
-              size="sm"
-              className="mt-10 text-xs md:w-full"
-              onClick={() => router.push("/studio")}
+              variant="secondary"
+              disabled={loading}
+              onClick={onLogout}
+              className="w-full flex items-center gap-x-5 bg-secondary text-secondary-foreground hover:bg-secondary/80 rounded-md text-sm font-medium p-3"
             >
-              <p className="hidden md:block">Go to Dashboard</p>
-              <LayoutDashboard className="h-5 w-5 md:hidden" />
+              <LogOut className="w-5 h-5" />
+              <p className="hidden md:block">Logout</p>
             </Button>
-          </SignedIn>
+          ) : (
+            <Link
+              href="/auth/sign-in"
+              className="flex items-center gap-x-5 bg-primary text-primary-foreground hover:bg-primary/90 rounded-md text-sm font-medium p-3"
+            >
+              <LogIn className="w-5 h-5" />
+              <p className="hidden md:block">Login</p>
+            </Link>
+          )}
         </div>
       </div>
     </nav>
--- app/(home)/components/navbar.tsx
+++ app/(home)/components/navbar.tsx
@@ -3,19 +3,19 @@ import { SearchToggle } from "@/components/search-toggle";
 import { HomeNavigationMenu } from "./nav-items";
 import { HomeMobileMenu } from "./mobile-menu";
 
-export function Navbar() {
+export function Navbar({ signedIn }: { signedIn: boolean }) {
   return (
     <>
       <div className="sm:ml-16 md:ml-52 border-b-[1px] shadow-sm h-16 flex items-center justify-between gap-x-2.5 px-4">
         <div className="block sm:hidden">
-          <HomeMobileMenu />
+          <HomeMobileMenu signedIn={signedIn} />
         </div>
         <div className="w-full flex justify-end gap-x-1">
           <SearchToggle />
           <ThemeToggle />
         </div>
       </div>
-      <HomeNavigationMenu />
+      <HomeNavigationMenu signedIn={signedIn} />
     </>
   );
 }
