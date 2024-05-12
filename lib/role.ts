09d33576463d8a99311d277106612e310f828b7b
haseakito <haseakito@gmail.com>
2024-05-12 15:36:41 +1000

    :fire: Remove `clerk` role based access checker

--- lib/role.ts
+++
@@ -1,9 +0,0 @@
-import { auth } from "@clerk/nextjs";
-import { Roles } from "@/types/globals";
-
-export const checkRole = (role: Roles) => {
-  // Retrieve session
-  const { sessionClaims } = auth();
-
-  return sessionClaims?.metadata.role === role;
-};
